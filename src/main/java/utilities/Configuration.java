0132e5de5a9e9d3b21ec681c9dec578590f1612c
Paul Merrill <paul.merrill@beaufortfairmont.com>
2017-04-23 18:58:31 -0400

    Changing configuration to local.

--- src/main/java/utilities/Configuration.java
+++ src/main/java/utilities/Configuration.java
@@ -2,9 +2,9 @@ package utilities;
 
 public class Configuration {
 //	public static String apiIp = "backend";
-	public static String apiIp = "34.206.240.114";
-	public static String apiPort = "8081";
+	public static String apiIp = "10.0.1.201";
+	public static String apiPort = "8082";
 //	public static String applicationIp = "frontend";
-	public static String applicationIp = "34.205.67.128";
+	public static String applicationIp = "10.0.1.201:8084/invoices";
 	public static String pathToChromeDriver = "/Users/dpm3354/working/DataStrategiesWorkspace/KylesWork/exampletests/src/main/resources/chromedriver";
 }
