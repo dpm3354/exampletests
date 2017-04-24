9683bb4b11b48f608ca131f2f5d69fe34b921373
Paul Merrill <paul.merrill@beaufortfairmont.com>
2017-04-24 09:23:07 -0400

    Changed driver to phantomjs

--- src/test/java/testcases/InvoiceTests.java
+++ src/test/java/testcases/InvoiceTests.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 import utilities.Configuration;
 
@@ -15,9 +16,11 @@ public class InvoiceTests {
 
 	public void setUp(){
 //		System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+//		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		System.setProperty("phantomjs.binary.path", "/usr/local/bin/phantomjs");
 		
-		driver = new ChromeDriver();
+//		driver = new ChromeDriver();
+		driver = new PhantomJSDriver();
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
